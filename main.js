ce477b8e66d71d1f77fb1e2401e16dd4fcd460d9
NYamil <121986227+NYamil@users.noreply.github.com>
2024-01-18 08:23:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+gsap.to("#bg", {
+    scrollTrigger: {
+        scrub: true
+    },
+    y: 200,
+    scale: 1.2
+})
+
+gsap.to("#jet", {
+    scrollTrigger: {
+        scrub: true
+    },
+    x: -200,
+    y: -1800,
+    scale: 1.2
+})
+
+gsap.to("#hulk", {
+    scrollTrigger: {
+        scrub: true
+    },
+    x: 1000,
+    y: 200,
+    scale: 1.5
+})
